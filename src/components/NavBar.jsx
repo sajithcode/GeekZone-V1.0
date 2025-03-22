1f8d4be2663458f57e8a9a0de22c452b72a99d16
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-22 12:14:19 +0530

    add custom css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import './navbar.css'
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
